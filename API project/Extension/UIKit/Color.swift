b2d6c6d4b05fdd7ab0404233fc3e0841e013b27c
Egor Syrtcov <sev_84@inbox.ru>
2020-03-09 13:24:25 +0300

    init new project

---
+++ API project/Extension/UIKit/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  API project
+//
+//  Created by Egor Syrtcov on 3/9/20.
+//  Copyright © 2020 Egor Syrtcov. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(red: CGFloat, green: CGFloat, blue: CGFloat) {
+        self.init(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    static func mainGreen() -> UIColor {
+        return UIColor(red: 124, green: 196, blue: 0)
+    }
+}
+
---
+++ API project/Extension/UIKit/UIViewControllerStoryboard.swift
@@ -0,0 +1,11 @@
+//
+//  UIViewControllerStoryboard.swift
+//  API project
+//
+//  Created by Egor Syrtcov on 3/9/20.
+//  Copyright © 2020 Egor Syrtcov. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController: StoryboardInitializable {}
